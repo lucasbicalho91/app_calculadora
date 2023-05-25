1cad9c480623e909d4632fa50b5310554916d061
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-05-25 20:56:43 -0300

    Criada a lógica para o valor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function calcular(tipo, valor) {
-  console.log(tipo, valor)
+  
+  if (tipo === 'acao') {
+
+  } else if (tipo === 'valor') {
+    var valor_campo = document.getElementById('resultado').value
+    document.getElementById('resultado').value = valor_campo + valor;
+  }
+
 }
