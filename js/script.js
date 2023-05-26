988c8bd4a3ba0af5d6256c8453c5548666cab91f
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-05-25 21:09:45 -0300

    Calculadora finalizada

--- js/script.js
+++ js/script.js
@@ -12,9 +12,14 @@ function calcular(tipo, valor) {
       document.getElementById('resultado').value = valor_campo + valor;
     }
 
+    if (valor === '=') {
+     var valor_campo =  eval(document.getElementById('resultado').value);
+     
+     document.getElementById('resultado').value = valor_campo;
+    }
+
   } else if (tipo === 'valor') {
     var valor_campo = document.getElementById('resultado').value
     document.getElementById('resultado').value = valor_campo + valor;
   }
-
 }
